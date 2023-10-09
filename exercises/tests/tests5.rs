f5ad6edb7587a4e6e23264368ae8763d81c6d187
Zera-Algorithm <2110459069@qq.com>
2023-10-09 20:57:19 +0800

    finish all

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -3,13 +3,13 @@
 // An `unsafe` in Rust serves as a contract.
 //
 // When `unsafe` is marked on an item declaration, such as a function,
-// a trait or so on, it declares a contract alongside it. However,
+// a trait or so on, it declares a contract(契约) alongside it. However,
 // the content of the contract cannot be expressed only by a single keyword.
 // Hence, its your responsibility to manually state it in the `# Safety`
 // section of your documentation comment on the item.
 //
 // When `unsafe` is marked on a code block enclosed by curly braces,
-// it declares an observance of some contract, such as the validity of some
+// it declares an observance of some contract(一些惯例的契约), such as the validity of some
 // pointer parameter, the ownership of some memory address. However, like
 // the text above, you still need to state how the contract is observed in
 // the comment on the code block.
@@ -22,7 +22,6 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 /// # Safety
 ///
@@ -32,7 +31,10 @@ unsafe fn modify_by_address(address: usize) {
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-        todo!("Your code goes here")
+        // SAFETY: The address is guaranteed to be valid and contains
+        // a unique reference to a `u32` local variable.
+        let p = address as *mut u32;
+        *p = 0xAABBCCDD;
     }
 }
 
